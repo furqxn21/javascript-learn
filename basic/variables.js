6c9a44269e03e9f60b74bd7f5c1b0ffcd906f3f0
furqxn21 <155625817+furqxn21@users.noreply.github.com>
2025-09-02 12:19:35 +0000

    variable&cons

---
+++ basic/variables.js
@@ -0,0 +1,12 @@
+const accountId = 291605
+let accountEmail="ft2916@gmail.com"
+var accountPassword= "291605"
+accountCity="Kolkata"
+/*
+prefer not to use var issue in block 
+*/
+accountEmail="ff29@gmail.com"
+accountPassword="12345"
+accountCity="Gaya"
+// accountId=2116 // not allowed
+console.table([accountId ,accountEmail, accountPassword, accountCity]);
